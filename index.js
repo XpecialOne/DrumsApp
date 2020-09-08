89ae9d177ade619724465f78fbf39d70d688b2a9
XpecialOne <sofiane.masaif@gmail.com>
2020-09-09 00:25:41 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,59 @@
+
+// Detecting HTML boutton press
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i=0; i < numberOfDrumButtons; i++ ) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+  } );
+}
+
+// Detecting keyboard key press
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    console.log("You pressed the " + "'" + event.key + "'" + " key");
+});
+
+//
+// Apply sound function
+function makeSound (key){
+
+switch (key){
+    case "a" :
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+        break;
+    case "z" :
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+        break;
+
+    case "e" :
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
+    case "r" :
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+    case "t" :
+        var snare = new Audio('sounds/snare.mp3');
+        snare.play();
+        break;
+    case "y" :
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+        break;
+    case "u" :
+        var kick = new Audio('sounds/kick-bass.mp3');
+        kick.play();
+        break;
+
+    default : console.log(buttonInnerHTML);
+
+};
+}
